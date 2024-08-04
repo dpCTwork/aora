c30aa66b075f3865a1d7e24fcb9feeb3af6849d1
David Park <davidpark.kh@gmail.com>
2024-08-03 22:03:38 -0400

    added some components, finished the authentication page UI, will add logic.

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,17 @@
 // Code: App component
 
-import { Link } from "expo-router"
+import { Link, router } from "expo-router"
 import { StatusBar } from "expo-status-bar"
 import { Image, ScrollView, Text, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { images } from "../constants"
+import CustomButton from "../components/CustomButton"
 
 export default function App() {
 	return (
 		<SafeAreaView className="bg-primary h-full">
 			<ScrollView contentContainerStyle={{ height: "100%" }}>
-				<View className="w-full flex justify-center items-center h-full px-4">
+				<View className="w-full flex justify-center items-center min-h-[90vh] px-4">
 					<Image
 						source={images.logo}
 						className="w-[130px] h-[84px]"
@@ -36,8 +37,16 @@ export default function App() {
 						Where creativity meets innovation: embark on a journey
 						of limitless exploration with Aora
 					</Text>
+					<CustomButton
+						title="Continue with Email"
+						handlePress={() => router.push("/sign-in")}
+						containerStyles="w-full mt-7"
+					/>
 				</View>
 			</ScrollView>
+
+			{/* the status bar on top of phone changed to light mode so it stands out against the dark background */}
+			<StatusBar backgroundColor="#161622" style="light" />
 		</SafeAreaView>
 	)
 }
